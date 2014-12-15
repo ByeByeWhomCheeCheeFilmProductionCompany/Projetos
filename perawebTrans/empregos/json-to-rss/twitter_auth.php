b4eabab62e840176fa5603da7cd9362c604757c1
rafapeartree <rroberto@igcorp.com.br>
2014-12-15 01:28:12 -0200

    troca do nome da pasta

--- perawebTrans/empregos/json-to-rss/twitter_auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-$twitter_auth = array(			// Copy and paste in your Twitter app's ...
-	'consumer_key'    => 'BN8xfMYE07Huvp0IaFatBA',	// consumer key
-	'consumer_secret' => '02fXGh08XbDu9MQzXoCuQmrEF48CckVgXNeQVhCzY',	// consumer secret
-	'user_token'      => '217982876-HuL7uKIFJQkwWBTqcuAPYfRKN5XOExWk1eLcGwuX',	// access token
-	'user_secret'     => 'mDY5NkOkmzdUsl4Q7xL0MG2gzKXvBzmlBgpQcGK8',	// access token secret
-	'curl_ssl_verifypeer'   => false
-)
-?>
